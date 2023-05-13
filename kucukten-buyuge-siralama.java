b288eabec4a22a2cdc939b415c7b452be73809ea
Ayşegül Koşar <aysegulkosar@outlook.com.tr>
2023-05-14 02:48:42 +0300

    Add files via upload

---
+++ kucukten-buyuge-siralama.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        int a, b, c;
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("A değerini Giriniz: ");
+        a = input.nextInt();
+
+        System.out.print("B değerini Giriniz: ");
+        b= input.nextInt();
+
+        System.out.print("C değerini Giriniz: ");
+        c = input.nextInt();
+
+        if ((a>b) && (a>c)) {
+            if (b > c) {
+                System.out.println("c<b<a");
+            } else {
+                System.out.println("b<c<a");
+            }
+        } else if ((b>a) && (b>c)) {
+            if (a > c) {
+                System.out.println("c < a < b");
+            } else {
+                System.out.print("a<c<b");
+            }
+        } else if ((c>a) && (c>b)) {
+            if (a>b) {
+                System.out.println("b<a<c");
+            } else {
+                System.out.println("a<b<c");
+            }
+        }
+    }
+}
